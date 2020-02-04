cccc8ad29cdc7e526445a46c058503b63273339e
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-05 02:58:41 +0300

    Refactoring `App`

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
 
-import './app.css';
+import "./app.css";
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  state = {
+    selectedPerson: null
+  }
+
+  onPersonSelected = (id) => {
+    console.log(`selectedPerson: ${id}`);
+    this.setState({
+      selectedPerson: id
+    })
+  }
+
+  render() {
+    const { selectedPerson } = this.state;
+
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={selectedPerson} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
