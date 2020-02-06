446e1b37752a76b71bb8178f2b030d385e00d534
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-07 00:38:57 +0300

    Improve `ItemList`

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -45,14 +45,12 @@ export default class ItemList extends Component {
   render() {
     const { peopleList } = this.state;
     
-    if (!peopleList) {
-      return <Spinner />;
-    }
-    
-    const items = this.renderItems(peopleList);
+    const spinner = !peopleList ? <Spinner /> : null;
+    const items = peopleList ? this.renderItems(peopleList) : null;
 
     return (
       <ul className="item-list list-group">
+        {spinner}
         {items}
       </ul>
     );
