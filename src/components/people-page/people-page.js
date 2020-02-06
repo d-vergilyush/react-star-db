cc0e8d3b1c884d8b59db782ed680aa99221c3785
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-07 00:42:52 +0300

    Update `PeoplePage`

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 
 import './people-page.css';
 
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+import ErrorIndicator from "../error-indicator";
+
 export default class PeoplePage extends Component {
+  state = {
+    selectedPersonId: null,
+    hasError: false
+  }
+
+  onPersonSelected = (selectedPersonId) => {
+    this.setState({
+      selectedPersonId
+    })
+  }
+
+  componentDidCatch(error) {
+    this.setState({
+      hasError: true
+    });
+  }
+
   render() {
+    const { selectedPersonId, hasError } = this.state;
+
+  if(hasError) {
+    return <ErrorIndicator />
+  };
+
     return (
-      <div>PeoplePage</div>
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList onItemSelected={this.onPersonSelected} />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails personId={selectedPersonId} />
+        </div>
+      </div>
     )
   }
 }
