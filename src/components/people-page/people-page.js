bd6658e8d50f4753b67ee06d814448e453e59bed
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-06 22:57:48 +0300

    Create component `PeoplePage`

---
+++ src/components/people-page/people-page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './people-page.css';
+
+export default class PeoplePage extends Component {
+  render() {
+    return (
+      <div>PeoplePage</div>
+    )
+  }
+}
