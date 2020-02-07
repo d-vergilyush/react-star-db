44fea85ba7e87b273328620f6a5c97ef7c8baafc
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-07 23:53:15 +0300

    Refactoring `App`

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 
+import "./app.css";
+
+import SwapiService from "../../services/swapi-service";
+
 import Header from "../header";
 import RandomPlanet from "../random-planet";
 import PeoplePage from "../people-page";
 import ErrorIndicator from "../error-indicator";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
 
-import "./app.css";
 
 export default class App extends Component {
+  swapiService = new SwapiService();
 
   state = {
+    selectedPersonId: null,
     hasError: false
   };
 
@@ -31,6 +38,30 @@ export default class App extends Component {
         <Header />
         <RandomPlanet />
         <PeoplePage />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList 
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+              renderItem={({ name, diameter }) => <span>{name} (<em>{diameter}</em>)</span> } />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPersonId} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList 
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+              renderItem={({ name, model }) => <span>{name} (<em>{model}</em>)</span> } />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPersonId} />
+          </div>
+        </div>
       </div>
     );
   }
