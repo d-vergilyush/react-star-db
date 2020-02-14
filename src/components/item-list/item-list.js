43c4d044a8739ecce81291aafd2580fb2872301c
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-15 00:21:28 +0300

    Refactoring `ItemList`. Create `withData`

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -3,56 +3,67 @@ import React, { Component } from "react";
 import "./item-list.css";
 
 import Spinner from "../spinner";
+import SwapiService from "../../services/swapi-service"
 
-export default class ItemList extends Component {
-  state = {
-    itemList: null
-  };
+const { getAllPeople } = new SwapiService();
 
-  componentDidMount() {
-    this.loadItemList();
-  }
+const ItemList = ({ onItemSelected, data, children}) => {
 
-  onItemListLoaded = itemList => {
-    this.setState({
-      itemList
-    });
-  };
-
-  loadItemList = () => {
-    const { getData } = this.props;
-
-    getData().then(this.onItemListLoaded);
-  };
-
-  renderItems(arr) {
-    return arr.map((item) => {
-      const { id } = item;
-      const label = this.props.children(item);
-
-      return (
-        <li
-          className="list-group-item"
-          key={id}
-          onClick={() => this.props.onItemSelected(id)}
-        >
-          {label}
-        </li>
-      );
-    });
-  }
-
-  render() {
-    const { itemList } = this.state;
-
-    const spinner = !itemList ? <Spinner /> : null;
-    const items = itemList ? this.renderItems(itemList) : null;
+  
+  
+  const items = data.map((item) => {
+    const { id } = item;
+    const label = children(item);
 
     return (
-      <ul className="item-list list-group">
-        {spinner}
-        {items}
-      </ul>
+      <li
+        className="list-group-item"
+        key={id}
+        onClick={() => onItemSelected(id)}
+      >
+        {label}
+      </li>
     );
+  });
+
+  return (
+    <ul className="item-list list-group">
+      {items}
+    </ul>
+  );
+}
+
+const withData = (View, getData) => {
+  return class extends Component {
+
+    state = {
+      data: null
+    };
+  
+    componentDidMount() {
+      this.loadData();
+    }
+  
+    onDataLoaded = data => {
+      this.setState({
+        data
+      });
+    };
+  
+    loadData = () => {
+      getData().then(this.onDataLoaded);
+    };
+
+    render() {
+      const { data } = this.state;
+
+      if(!data) {
+        return <Spinner />;
+      }
+
+      return <View {...this.props} data={data} />
+    }
   }
 }
+
+export default withData(ItemList, getAllPeople);
