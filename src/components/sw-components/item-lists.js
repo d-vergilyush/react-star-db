6a95bdb7444e7677dfa90b62c608bb1b48ac0aaa
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-16 16:12:56 +0300

    Fix `item-lists`

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -9,7 +9,7 @@ const {
   getAllStarships
 } = new SwapiService();
 
-const withChildData = (Wrapped, fn) => {
+const withChildFunction = (Wrapped, fn) => {
   return (props) => {
     return (
       <Wrapped {...props}>
@@ -19,13 +19,14 @@ const withChildData = (Wrapped, fn) => {
   };
 };
 
-const ListWithChild = withChildData(ItemList, ({ name }) => <span>{name}</span>);
+const renderName = ({ name }) => <span>{name}</span>;
+const renderModelAndName = ({ name, model }) => <span>{name} ({model})</span>;
 
-const PersonList = withData(ListWithChild, getAllPeople);
+const PersonList = withData(withChildFunction(ItemList, renderName), getAllPeople);
 
-const PlanetList = withData(ListWithChild, getAllPlanets);
+const PlanetList = withData(withChildFunction(ItemList, renderName), getAllPlanets);
 
-const StarshipList = withData(ListWithChild, getAllStarships);
+const StarshipList = withData(withChildFunction(ItemList, renderModelAndName), getAllStarships);
 
 export {
   PersonList,
