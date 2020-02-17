0fb4e35c944f296d64d3b39b59158a23998439d2
Dana Vergilyush <dvergilyush@gmail.com>
2020-02-17 19:40:56 +0300

    Update `Header`. Add btn fot Change service

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import "./header.css";
 
-const Header = () => {
+const Header = ({onServiceChange}) => {
   return (
     <div className="header d-flex">
       <h3>
@@ -20,6 +20,12 @@ const Header = () => {
           <a href="#">Starships</a>
         </li>
       </ul>
+      <button
+        className="btn btn-primary btn-sm"
+        onClick={onServiceChange}
+      >
+        Change service
+      </button>
     </div>
   );
 };
