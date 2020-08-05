c39137abe4bca4139e6dfec4120ac336be002889
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-06 00:45:09 +0300

    Edit `withData`

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Spinner from "../spinner";
 import ErrorIndicator from "../error-indicator";
 
-const withData = View => {
+const withData = (View) => {
   return class extends Component {
     state = {
       data: null,
